6578174c7329ba4f4d3033f1ea94d494812b7b1a
gjparts <gjparts@gmail.com>
2025-05-22 09:06:10 -0600

    commit 220520253078

---
+++ 00_primerdia/src/pkg00_primerdia/Main.java
@@ -0,0 +1,36 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package pkg00_primerdia;
+
+/**
+ *
+ * @author Gerardo
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // comentario de un renglon
+        /*
+        comentario
+        de
+        varios renglones
+        */
+        //imprimir texto en la consola
+        //println: imprime un String y pasa a la linea siguiente
+        System.out.println("Hola UNAH");
+        System.out.println("Gerardo");
+        System.out.println("Ingenieria en Sistemas");
+        //print: imprime un String y se queda en la linea actual
+        System.out.print("Verde");
+        System.out.print("Rojo");
+        System.out.print("Azul");
+        System.out.println("UNAH CORTES");
+        System.out.println("Adios");
+    }
+    
+}
