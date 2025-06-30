c11f94164d86f2bbbe8675b261634a5811e25ea1
gjparts <gjparts@gmail.com>
2025-06-30 10:58:26 -0600

    commit 3006202518723

--- 30_String01/src/pkg30_string01/Main.java
+++ 30_String01/src/pkg30_string01/Main.java
@@ -52,5 +52,71 @@ public class Main {
         for( int i = 0; i < comida.length(); i++ ){
             System.out.println( comida.charAt(i) );
         }
+        
+        //transformaciones de String
+        //obtener una version en mayusculas o minusculas de un String
+        //metodos a usar: toUpperCase() toLowerCase()
+        //estos metodos no afectan al String original
+        String ciudad = "San Pedro Sula";
+        System.out.println("ciudad en mayusculas: "+ciudad.toUpperCase());
+        System.out.println("ciudad en minusculas: "+ciudad.toLowerCase());
+        System.out.println("ciudad (String original): "+ciudad);
+        //si desea alterar al String original, simplente los sobre-escribe con
+        //su version mayuscula o minuscula.
+        ciudad = ciudad.toUpperCase();
+        System.out.println("ciudad (String original): "+ciudad);
+        ciudad = ciudad.toLowerCase();
+        System.out.println("ciudad (String original): "+ciudad);
+        
+        /*metodo trim()
+        devuelve una version del String sin los espacios iniciales y finales
+        que son conocidos como espacios de rellemo.
+        -> Este metodo no afecta al String original.*/
+        String juguete = "     Figura de Accion                        ";
+        System.out.println("juguete (original): "+juguete+".");
+        System.out.println("longitud de juguete (original): "+juguete.length());
+        //impresion aplicando trim()
+        System.out.println("juguete (trim): "+juguete.trim()+".");
+        System.out.println("longitud de juguete (trim): "+juguete.trim().length());
+        //se habra afectado la variable original?
+        System.out.println("juguete (original): "+juguete+".");
+        //se quiere afectar a la variable original:
+        juguete = juguete.trim();
+        System.out.println("juguete (original): "+juguete+".");
+        
+        String platillo = "     Pizza Super Suprema           ";
+        /*Ejercicio: imprima la variable platillo; pero sin relleno de
+        espacios y en mayusculas sin alterar a la variable original usando
+        una sola instruccion.*/
+        System.out.println( platillo.trim().toUpperCase() ); //forma 1
+        System.out.println( platillo.toUpperCase().trim() ); //forma 2
+        
+        //Concatenar valores char a un String
+        String carro = "Toyota Corolla 2012";
+        //concatenar un char sin afectar la variable original:
+        System.out.println(carro+'L');
+        System.out.println("carro (original): "+carro);
+        //concantear un char afectando a la variable original:
+        carro = carro+'L';
+        System.out.println("carro (original): "+carro);
+        //concatenar varios char a un String afectando la variable original
+        String cliente = "Filomeno Colinas";
+        cliente = cliente+'A'+'B'+'C';
+        System.out.println("cliente (original): "+cliente);
+        //pero en esos casos es mas practico concatenar otro String:
+        String estudiante = "Estevancito Maravilla";
+        estudiante = estudiante+"ABC";
+        System.out.println("estudiante (original): "+estudiante);
+        //hay situaciones donde puede tener el char en una variable aparte:
+        char equis = 'X';
+        String proveedor = "Curco Bain";
+        proveedor = proveedor+equis;
+        System.out.println("proveedor (original): "+proveedor);
+        //lo que no conviene hacer:
+        //concatenar varios char usando un operador de incremento
+        String mascota = "Firulais";
+        mascota += 'Z'; //esto si se puede, es como decir: mascota = mascota + 'Z'
+        mascota += 'W'+'X'+'R'; //esto no funciona bien
+        System.out.println("mascota (original): "+mascota);
     }    
 }
