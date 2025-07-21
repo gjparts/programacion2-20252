bc8358f1b41f788651d8006ae785318e261cafb4
gjparts <gjparts@gmail.com>
2025-07-21 10:57:03 -0600

    commit 0721202512510

---
+++ 44_gui03/src/pkg44_gui03/Main.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package pkg44_gui03;
+
+/**
+ *
+ * @author Gerardo
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        //hacer un objeto de clase Sumadora
+        Sumadora algo = new Sumadora();
+        algo.setVisible(true);
+    }
+    
+}
