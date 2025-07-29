8c559b14f7faa7cad8a97f0abccbed470e950904
gjparts <gjparts@gmail.com>
2025-07-29 10:51:38 -0600

    commit 072920257590

---
+++ 48_gui07/src/pkg48_gui07/Main.java
@@ -0,0 +1,12 @@
+package pkg48_gui07;
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Ventana1 z = new Ventana1();
+        z.setVisible(true);
+    }
+    
+}
